05bb53904623052668aa3438836fece924ba55ff
yankj12 <yankj12@163.com>
2018-04-03 23:31:46 +0800

    tomcat8之后修改了新的websocket方式

--- src/main/java/com/webscoletdemo/action/WebScoketAction.java
+++ src/main/java/com/webscoletdemo/action/WebScoketAction.java
@@ -1,59 +1,100 @@
 package com.webscoletdemo.action;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
-import java.util.ArrayList;
-import java.util.List;
-import javax.servlet.http.HttpServletRequest;
-import javax.xml.ws.WebServiceFeature;
-import org.apache.catalina.websocket.MessageInbound;
-import org.apache.catalina.websocket.WebSocketServlet;
-import org.apache.catalina.websocket.WsOutbound;
+import java.util.concurrent.CopyOnWriteArraySet;
 
-public class WebScoketAction extends WebSocketServlet {  
-    private static final long serialVersionUID = -4853540828121130946L;  
-    private static List<MyMessageInbound> mmiList = new ArrayList<MyMessageInbound>();  
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
 
-    @Override  
-    protected MyMessageInbound createWebSocketInbound(String str,  
-            HttpServletRequest request) {  
-        request.getParameter("");       
-        return new MyMessageInbound();  
-    }  
+/**
+*  参考自 https://www.cnblogs.com/xdp-gacl/p/5193279.html
+**/
+@ServerEndpoint("/websocket")
+public class WebScoketAction {  
+    //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
+    private static int onlineCount = 0;
 
-    private class MyMessageInbound extends MessageInbound {  
-        WsOutbound myoutbound;  
-        //开启websocket
-        public void onOpen(WsOutbound outbound) {  
-            try {  
-                System.out.println("Open Client.");  
-                this.myoutbound = outbound;  
-                mmiList.add(this);  
-                outbound.writeTextMessage(CharBuffer.wrap("Hello!"));  
-            } catch (IOException e) {  
-                e.printStackTrace();  
-            }  
-        }  
+    //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。若要实现服务端与单一客户端通信的话，可以使用Map来存放，其中Key可以为用户标识
+    private static CopyOnWriteArraySet<WebScoketAction> webSocketSet = new CopyOnWriteArraySet<WebScoketAction>();
 
-        //关闭websocket
-        public void onClose(int status) {  
-            System.out.println("Close Client.");  
-            mmiList.remove(this);  
-        }  
-       //当服务器端收到信息的时候,就对所有的连接进行遍历并且把收到的信息发送给所有用户  
-        public void onTextMessage(CharBuffer cb) throws IOException {  
-            System.out.println("Accept Message : " + cb);  
-            for (MyMessageInbound mmib : mmiList) {  
-                CharBuffer buffer = CharBuffer.wrap(cb);  
-                mmib.myoutbound.writeTextMessage(buffer);  
-                mmib.myoutbound.flush();  
-            }  
-        }  
+    //与某个客户端的连接会话，需要通过它来给客户端发送数据
+    private Session session;
 
+    /**
+     * 连接建立成功调用的方法
+     * @param session  可选的参数。session为与某个客户端的连接会话，需要通过它来给客户端发送数据
+     */
+    @OnOpen
+    public void onOpen(Session session){
+        this.session = session;
+        webSocketSet.add(this);     //加入set中
+        addOnlineCount();           //在线数加1
+        System.out.println("有新连接加入！当前在线人数为" + getOnlineCount());
+    }
 
-        public void onBinaryMessage(ByteBuffer bb) throws IOException {  
+    /**
+     * 连接关闭调用的方法
+     */
+    @OnClose
+    public void onClose(){
+        webSocketSet.remove(this);  //从set中删除
+        subOnlineCount();           //在线数减1
+        System.out.println("有一连接关闭！当前在线人数为" + getOnlineCount());
+    }
 
-        }  
-    }  
+    /**
+     * 收到客户端消息后调用的方法
+     * @param message 客户端发送过来的消息
+     * @param session 可选的参数
+     */
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        System.out.println("来自客户端的消息:" + message);
+        //群发消息
+        for(WebScoketAction item: webSocketSet){
+            try {
+                item.sendMessage(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+                continue;
+            }
+        }
+    }
+
+    /**
+     * 发生错误时调用
+     * @param session
+     * @param error
+     */
+    @OnError
+    public void onError(Session session, Throwable error){
+        System.out.println("发生错误");
+        error.printStackTrace();
+    }
+
+    /**
+     * 这个方法与上面几个方法不一样。没有用注解，是根据自己需要添加的方法。
+     * @param message
+     * @throws IOException
+     */
+    public void sendMessage(String message) throws IOException{
+        this.session.getBasicRemote().sendText(message);
+        //this.session.getAsyncRemote().sendText(message);
+    }
+
+    public static synchronized int getOnlineCount() {
+        return onlineCount;
+    }
+
+    public static synchronized void addOnlineCount() {
+    	WebScoketAction.onlineCount++;
+    }
+
+    public static synchronized void subOnlineCount() {
+    	WebScoketAction.onlineCount--;
+    }
 }  
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,61 +1,67 @@
-<%@ page language="java" import="java.util.*" pageEncoding="utf-8"%>
-<%
-String path = request.getContextPath();
-String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
-%>
-
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<%@ page language="java" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
 <html>
-  <head>
-    <base href="<%=basePath%>">
+<head>
+    <title>Java后端WebSocket的Tomcat实现</title>
+</head>
+<body>
+    Welcome<br/><input id="text" type="text"/>
+    <button onclick="send()">发送消息</button>
+    <hr/>
+    <button onclick="closeWebSocket()">关闭WebSocket连接</button>
+    <hr/>
+    <div id="message"></div>
+</body>
 
-    <title>My JSP 'index.jsp' starting page</title>
-    <meta http-equiv="pragma" content="no-cache">
-    <meta http-equiv="cache-control" content="no-cache">
-    <meta http-equiv="expires" content="0">    
-    <meta http-equiv="keywords" content="keyword1,keyword2,keyword3">
-    <meta http-equiv="description" content="This is my page">
-    <!--
-    <link rel="stylesheet" type="text/css" href="styles.css">
-    -->
-    <script type="text/javascript">
-var socket = new WebSocket("ws://localhost:8080/message/aa.do?aaa=11");
-socket.onopen = function(){
-    //浏览器socket开始进入
-    console.log("open");
-    //向服务器端发送信息
-    socket.send("open+hello");
-};
-socket.onmessage = function(evt)
-{
-    console.log("message");
-    console.log("onmessage:"+evt.data);
-};
-socket.onclose = function(evt){
-    console.log("close");
-    console.log("onclose:"+evt.data);
-};
-socket.onerror = function(evt)
-{
-    console.log("error");
-    console.log("onerror:"+evt.data);
-};
-function send()
-{
-    //socket.send("sendMessage");
-}
-function closeWebSocket(){
-     socket.close();
-}
+<script type="text/javascript">
+    var websocket = null;
+    //判断当前浏览器是否支持WebSocket
+    if ('WebSocket' in window) {
+        websocket = new WebSocket("ws://localhost:8080/message/websocket");
+    }
+    else {
+        alert('当前浏览器 Not support websocket')
+    }
 
+    //连接发生错误的回调方法
+    websocket.onerror = function () {
+        setMessageInnerHTML("WebSocket连接发生错误");
+    };
+
+    //连接成功建立的回调方法
+    websocket.onopen = function () {
+        setMessageInnerHTML("WebSocket连接成功");
+    }
+
+    //接收到消息的回调方法
+    websocket.onmessage = function (event) {
+        setMessageInnerHTML(event.data);
+    }
+
+    //连接关闭的回调方法
+    websocket.onclose = function () {
+        setMessageInnerHTML("WebSocket连接关闭");
+    }
+
+    //监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
+    window.onbeforeunload = function () {
+        closeWebSocket();
+    }
+
+    //将消息显示在网页上
+    function setMessageInnerHTML(innerHTML) {
+        document.getElementById('message').innerHTML += innerHTML + '<br/>';
+    }
+
+    //关闭WebSocket连接
+    function closeWebSocket() {
+        websocket.close();
+    }
+
+    //发送消息
+    function send() {
+        var message = document.getElementById('text').value;
+        websocket.send(message);
+    }
 </script>
-  </head>
-
-  <body>
-    This is my JSP page. <br>
-     Welcome<br/>
-   <button onclick="send()">Send</button>    <button onclick="closeWebSocket()">Close</button>
-    <div id="message">
-    </div>
-  </body>
 </html>
